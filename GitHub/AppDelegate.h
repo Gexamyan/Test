5abf56781e333a06a6c23ecbc84bfeca857679d5
seryozha.movsisyan <seryozha.movsisyan@picsart.com>
2016-07-26 12:08:46 +0400

    Initial Commit

---
+++ GitHub/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitHub
+//
+//  Created by Seryozha Movsisyan on 7/26/16.
+//  Copyright © 2016 Seryozha Movsisyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
